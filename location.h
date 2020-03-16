d35e6f28fce4e81372a78d1d4c6f7efe105ba7cc
ezhang7423 <ezhang7423@student.palomar.edu>
2020-03-15 19:23:35 -0700

    make some getters

--- location.h
+++ location.h
@@ -12,27 +12,40 @@ struct locComp
 };
 
 typedef std::set<std::pair<Location, int>, locComp> link;
+/* list of all other places
+  linked to this one. each holds the other location, and how many
+  people are flowing from that city to this one. that number can be negative.
+  because this is a set, the cities which flow the most to this one are automatically
+  at the top of the list 
+  */
 
 class Location
 {
 public:
-    Location(int total, int infected)
+    Location(std::string name, double x, double y, int total, int infected)
     {
+        this->name = name;
         people["infected"] = infected;
         people["total"] = total;
         people["healthy"] = total - infected;
         people["recovered"] = 0;
         people["dead"] = 0;
+        coords.first = x;
+        coords.second = y;
+    };
+
+    std::string getName()
+    {
+        return name;
+    }
+    std::pair<double, double> getCoords()
+    {
+        return coords;
     }
 
 private:
+    std::string name;
     std::unordered_map<std::string, int> people;
-    // int infected;
-    // int healthy;
-    // int recovered;
-    // int dead;
-    // int total;
-    int velocityIn;
-    int velocityOut;
+    std::pair<double, double> coords;
     link links; // link
 };
