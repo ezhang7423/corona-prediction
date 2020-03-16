98486059407ac3c89d067af364f06492a3436b01
ezhang7423 <ezhang7423@student.palomar.edu>
2020-03-15 21:58:33 -0700

    airport data

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 header: locations/city.h
 	g++ -c $^
+
+clean:
+	find . -type f -name '*.o' -delete && find . -type f -name '*.gch' -delete
--- main.cpp
+++ main.cpp
@@ -4,5 +4,17 @@
 using namespace std;
 int main()
 {
-    can i do this ?
+    // just a data store for now
 }
+
+/*
+entering ny:
+2 big airports
+
+total:
+104636862
+or 286676 per day
+ny pop:
+19453561
+in other words, 1.4% of population travels per day in city
+*/
